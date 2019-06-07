2a620be14f7948cf4591e6f062d4b3b7014c092e
Isco <Iscocuesta@hotmail.com>
2019-06-07 09:47:52 -0500

    routes working and input.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var path = require("path");
+
+// Create express app instance.
+var app = express();
+
+var router = express.Router()
+
+
+
+
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+router.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+
+module.exports = router;
+
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const htmlRouter = require('./app/routing/htmlRoutes')
+// const apiRouter = require('./app/routing/apiRoutes')
+
+app.use('/', htmlRouter);
+// app.use('/survey', apiRouter);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
